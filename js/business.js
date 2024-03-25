675b16c7c8ded7e49da71444b03c52fab9f16331
coolcool <159146271+limseewon@users.noreply.github.com>
2024-03-25 17:59:42 +0900

    add slide

--- js/business.js
+++ js/business.js
@@ -1,11 +1,10 @@
-var swiper = new Swiper(".mySwiper", {
-	pagination: {
-	  el: ".swiper-pagination",
-	  type: "fraction",
-	},
-	navigation: {
-	  nextEl: ".swiper-button-next",
-	  prevEl: ".swiper-button-prev",
-	},
-  });
-  
+const slider = $('.slider');
+
+$(document).on('click', activate);
+
+function activate(e){
+  const items = slider.find('.item');
+
+  e.target.matches('.next') && slider.append(items.eq(0));
+  e.target.matches('.prev') && slider.prepend(items.eq(items.length -1)); 
+}
