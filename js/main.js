2b59a63ad4fcaa82bfa760028993877d30d1b2ec
WooYeji <131832439+dubumom@users.noreply.github.com>
2024-03-26 14:42:04 +0900

    main add

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 //prcenter
 // 위로
-let dTop = -230;
+let dTop = -250;
 
 function moveTop(){
 
-  $('.design_film> .des_film01').css('margin-top',dTop);
+  $('.autoSlide > .slideUp').css('margin-top',dTop);
   dTop-=2; 
 
-  if(dTop==-232){
-  $('.design_film> .des_film01> li:first-child').insertAfter('.design_film> .des_film01 li:last-child');
-  }else if(dTop==-460){
-    dTop=-230;
+  if(dTop==-252){
+  $('.autoSlide > .slideUp > li:first-child').insertAfter('.autoSlide > .slideUp li:last-child');
+  }else if(dTop==-500){
+    dTop=-250;
   }
 };
 
 let Timer02 = setInterval(moveTop, 60);
 
 //마우스 오버 시 멈추게 하고 아웃 시 시간을 생성하여 움직이기게
-$('.design_film> .des_film01').hover(function(){
+$('.autoSlide > .slideUp').hover(function(){
   clearInterval(Timer02);
 },function(){
   clearInterval(Timer02);
@@ -26,25 +26,25 @@ $('.design_film> .des_film01').hover(function(){
 
 //아래로 
 
-let dBottom = -230;
+let dBottom = -250;
 
 function moveBottom(){
 
-  $('.design_film> .des_film02').css('margin-bottom',dBottom);
+  $('.autoSlide> .slideDown').css('margin-bottom',dBottom);
   dBottom -= 2; 
   // console.log(dBottom);
 
-  if(dBottom == -232){
-  $('.design_film> .des_film02> li:last-child').insertBefore('.design_film> .des_film02 li:first-child');
-  }else if(dBottom == -460){
-    dBottom = -230;
+  if(dBottom == -252){
+  $('.autoSlide> .slideDown > li:last-child').insertBefore('.autoSlide > .slideDown li:first-child');
+  }else if(dBottom == -500){
+    dBottom = -250;
   }
 };
 
 let Timer03 = setInterval(moveBottom, 60);
 
 //마우스 오버 시 멈추게 하고 아웃 시 시간을 생성하여 움직이기게
-$('.design_film> .des_film02').hover(function(){
+$('.autoSlide> .slideDown').hover(function(){
   clearInterval(Timer03);
 },function(){
   clearInterval(Timer03);
