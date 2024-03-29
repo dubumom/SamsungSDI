1bd639b3216a8f21e5438897a2b306cbad3394a2
WooYeji <131832439+dubumom@users.noreply.github.com>
2024-03-29 17:46:55 +0900

    modal js 추가

--- js/common.js
+++ js/common.js
@@ -1,4 +1,3 @@
-
 $('#header').load('header.html', headerJs);
 $('#footer').load('footer.html', footerJs);
 
@@ -214,53 +213,4 @@ footerslideWrapper.mouseleave(function(){
 autoSlide();
 */
 
-		
 }
-
-/* ---------- modal : cookie ---------- */
-
-const modal = document.querySelector('.modal');
-const input = modal.querySelector('input[type="checkbox"]');
-const modal_close =modal.querySelector('.modal_close');
-
-
-modal_close.addEventListener('click',(e)=>{
-e.preventDefault();
-// console.log(in5660put.checked);
-if(input.checked){
-//쿠키생성
-setCookie('Portfolio','samsung', 1);
-}else{
-//쿠키삭제
-delCookie('Portfolio');
-}
-modal.classList.add('hide');
-});
-function setCookie(name, val, day){
-let date = new Date();
-date.setDate(date.getDate()+day);
-document.cookie = `${name}=${val};Expires=${date}`;
-}
-function delCookie(name){
-let date = new Date();
-date.setDate(date.getDate()-1);
-document.cookie = `${name}='';Expires=${date}`;
-}
-
-function checkCookie(name){
-let cookieArr = document.cookie.split(';');
-let visited = false;
-
-for(let cookie of cookieArr){
-  if(cookie.indexOf(name) > -1) {
-	visited = true;
-  }
-}
-if(visited) {
-  modal.classList.add('hide');
-} else {
-  modal.classList.remove('hide');
-}
-}
-checkCookie('samsung');
-
