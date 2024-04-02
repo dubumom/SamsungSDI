523d9f399d439b60a5f2fdc280493f00e2f44f4c
BRI <159090432+briannayoo@users.noreply.github.com>
2024-04-02 13:10:29 +0900

    add inquery

--- js/inquery.js
+++ js/inquery.js
@@ -7,6 +7,14 @@ $("#msgForm").validate({
         useremail: {
             required: true,
             email: true
+        },
+        msg:{
+            required: true,
+            msg:true
+        },
+        inquery_title:{
+            required: true,
+            minlength: 2
         }
     },
     messages:{
@@ -17,6 +25,15 @@ $("#msgForm").validate({
         useremail: {
             required: '이메일은 필수입니다.',
             email: '이메일 형식에 맞춰 입력해주세요'
+        },
+        msg:{
+            required: '문의내용은 필수입니다.',
+            msg: '문의내용을 입력해주세요',
+            minlength: '최소 열 글자 이상 입력해주세요.'
+        },
+        inquery_title:{
+            required: '제목은 필수입니다.',
+            minlength: '최소 두 글자 이상 입력해주세요.'
         }
     },
     errorClass:'bad-input',
