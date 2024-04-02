a7973dc039ca85a92074c6d96677be46778b7ee4
WooYeji <131832439+dubumom@users.noreply.github.com>
2024-04-02 13:22:20 +0900

    add

--- js/R&D.js
+++ js/R&D.js
@@ -1,108 +1,36 @@
-/*
 const $tabMenu = $('#sub_header ul li');
 const $tabContent = $('.sec_content > section');
 const $highLight = $('.sHeaderActive');
-
-$tabMenu.each(function(idx) {
-	$(this).click(function(e) {
-		e.preventDefault();
-		scrollToSection(idx);
-		moveHighlight(idx);
-	});
-});
-
-$(window).scroll(function() {
-	const scrollTop = $(window).scrollTop();
-	$tabContent.each(function(idx) {
-		if ($(this).offset().top <= scrollTop) {
-			moveHighlight(idx);
-		}
-	});
-});
-
-function scrollToSection(num) {
-	const targetSection = $tabContent.eq(num);
-	const targetOST = targetSection.offset().top;
-	$('html, body').animate({scrollTop: targetOST}, 'slow');
-}
-
-function moveHighlight(num) {
-	const newTop = $tabMenu.eq(num).position().top;
-	$highLight.css({
-		'top': newTop + 'px',
-	});
-}
-*/
-const $tabMenu = $('#sub_header ul li');
-const $tabContent = $('.sec_content > section');
-const $highLight = $('.sHeaderActive');
-/*
-$tabMenu.each(function(idx) {
-	$(this).click(function(e) {
-		e.preventDefault();
-		scrollToSection(idx);
-		moveHighlight(idx);
-	});
-});
-
-$(window).scroll(function() {
-	const scrollTop = $(window).scrollTop();
-	$tabContent.each(function(idx) {
-		const sectionTop = $(this).offset().top;
-		const sectionBottom = sectionTop + $(this).outerHeight();
-		if (scrollTop >= sectionTop && scrollTop < sectionBottom) {
-			moveHighlight(idx);
-		}
-	});
-});
-
-function scrollToSection(num) {
-	const targetSection = $tabContent.eq(num);
-	const targetOST = targetSection.offset().top;
-	$('html, body').stop().animate({scrollTop: targetOST}, 'slow');
-}
-
-function moveHighlight(num) {
-	const newTop = $tabMenu.eq(num).position().top;
-	$highLight.css({
-		'top': newTop + 'px',
-	});
-}
-*/
 
 $tabMenu.click(function(e){
-	e.preventDefault();
-	scrollToSection($(this).index());
-		 //moveHighlight($(this).index());
-})
-
+    e.preventDefault();
+    scrollToSection($(this).index());
+});
 
 $(window).scroll(function() {
-	const scrollTop = $(window).scrollTop();
-	$tabContent.each(function(idx) {
-		 const sectionTop = $(this).offset().top;
-		 //const sectionBottom = sectionTop + $(this).outerHeight();
-		 if (scrollTop >= sectionTop - 5) {
-				console.log(scrollTop, sectionTop);
-				console.log(idx);
-				moveHighlight(idx);
-		 }
-	});
+    const scrollTop = $(window).scrollTop();
+    if ($(window).width() > 768) { // 윈도우 사이즈가 768px 이상인 경우에만 실행
+        $tabContent.each(function(idx) {
+            const sectionTop = $(this).offset().top;
+            if (scrollTop >= sectionTop - 5) {
+                moveHighlight(idx);
+            }
+        });
+    }
 });
 
 function scrollToSection(num) {
-	const targetSection = $tabContent.eq(num);
-	const targetOST = targetSection.offset().top;
-	console.log(targetOST);
-	$('html, body').animate({scrollTop: targetOST}, 'swing');
+    const targetSection = $tabContent.eq(num);
+    const targetOST = targetSection.offset().top;
+    $('html, body').animate({scrollTop: targetOST}, 'swing');
 }
 
 function moveHighlight(num) {
-	const newTop = $tabMenu.eq(num).position().top;
-	const newWidth = $tabMenu.eq(num).find('a').width() + 50;
+    const newTop = $tabMenu.eq(num).position().top;
+    const newWidth = $tabMenu.eq(num).find('a').width() + 50;
 
-	$highLight.css({
-		 'top': newTop + 'px',
-		 'width': newWidth + 'px',
-	});
+    $highLight.css({
+        'top': newTop + 'px',
+        'width': newWidth + 'px',
+    });
 }
